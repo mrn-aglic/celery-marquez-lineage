0c2c8755c05da8a98a1d3f32213e42e41f14ec4a
Marin Aglic <mrn.aglic@gmail.com>
2024-09-05 13:55:25 +0200

    Setup project skeleton

---
+++ README.md
@@ -0,0 +1,1 @@
+# Celery Marquez lineage example
---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo $1
+
+rm logs.txt
+
+if [ "$1" = 'scheduler' ]
+then
+    exec celery -A lineage.beat beat --loglevel info
+elif [ "$1" = 'worker' ]
+then
+    exec celery -A lineage.worker worker -Q default --loglevel info
+elif [ "$1" = 'flower' ]
+then
+    exec celery -A lineage.worker --broker=redis://redis:6379/0 flower --conf=/config/flowerconfig.py
+fi
+
+exec "$@"
