8ebca4c57dde2b7856edaa2fc31e72d43ff98486
Marin Aglic <mrn.aglic@gmail.com>
2024-09-11 19:53:52 +0200

    Add marquez services to docker compose

--- lineage/app/config.py
+++ lineage/app/config.py
@@ -13,3 +13,9 @@ def is_scheduler():
 
 def is_dev():
     return ENVIRONMENT == DEV_NAME
+
+
+MARQUEZ_HOST = environ.get("MARQUEZ_HOST", "marquez")
+MARQUEZ_PORT = environ.get("MARQUEZ_PORT", 9000)
+
+MARQUEZ_URL = f"http://{MARQUEZ_HOST}:{MARQUEZ_PORT}"
